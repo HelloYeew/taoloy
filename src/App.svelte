59a6ee1d5c0b1e36766f4ef5b7c6f0d37df4703d
Pontakorn Paesaeng <37771900+pontakornth@users.noreply.github.com>
2022-04-12 16:22:49 +0700

    Fix random button

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,8 @@
 	import CoffeeRecipe from "./CoffeeRecipe.svelte";
 </script>
 
-<style>
-
-</style>
-
 <h1>Taoloy</h1>
 <h2>Don't know what coffee you want to drink today?</h2>
 <h2>Let us think!</h2>
 
 <CoffeeRecipe />
-
-<button type="button" class="btn btn-primary" on:click={CoffeeRecipe}>Random a new one!</button>
--- src/CoffeeRecipe.svelte
+++ src/CoffeeRecipe.svelte
@@ -2,27 +2,38 @@
     // make a list of coffee recipes
     let recipes = [
         {
-            name: 'Cappuccino',
-            ingredients: ['Espresso', 'Steamed Milk', 'Whipped Cream'],
-            instructions: 'Combine Espresso, Steamed Milk and Whipped Cream in a pot'
+            name: "Cappuccino",
+            ingredients: ["Espresso", "Steamed Milk", "Whipped Cream"],
+            instructions:
+                "Combine Espresso, Steamed Milk and Whipped Cream in a pot",
         },
         {
-            name: 'Latte',
-            ingredients: ['Espresso', 'Steamed Milk'],
-            instructions: 'Combine Espresso and Steamed Milk in a pot'
+            name: "Latte",
+            ingredients: ["Espresso", "Steamed Milk"],
+            instructions: "Combine Espresso and Steamed Milk in a pot",
         },
         {
-            name: 'Mocha',
-            ingredients: ['Espresso', 'Steamed Milk', 'Whipped Cream', 'Chocolate Syrup'],
-            instructions: 'Combine Espresso, Steamed Milk, Whipped Cream and Chocolate Syrup in a pot'
-        }
+            name: "Mocha",
+            ingredients: [
+                "Espresso",
+                "Steamed Milk",
+                "Whipped Cream",
+                "Chocolate Syrup",
+            ],
+            instructions:
+                "Combine Espresso, Steamed Milk, Whipped Cream and Chocolate Syrup in a pot",
+        },
     ];
     // random coffee recipe
     let recipe = recipes[Math.floor(Math.random() * recipes.length)];
     let ingredients = recipe.ingredients;
 
     function randomRecipe() {
-        recipe = recipes[Math.floor(Math.random() * recipes.length)];
+        let newRecipe;
+        do {
+            newRecipe = recipes[Math.floor(Math.random() * recipes.length)];
+        } while (newRecipe == recipe);
+        recipe = newRecipe;
         ingredients = recipe.ingredients;
     }
 </script>
@@ -37,3 +48,6 @@
 </ul>
 <h3>Instructions</h3>
 <p>{recipe.instructions}</p>
+<button type="button" class="btn btn-primary" on:click={randomRecipe}
+    >Random a new one!</button
+>
